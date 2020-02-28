d9d030e412525dce32c08d447ed7d7a284a1182d
Burhan Budak <budak.burhan@gmail.com>
2020-02-28 23:24:32 +0100

    scroll added

--- client/src/components/Chatbox.js
+++ client/src/components/Chatbox.js
@@ -1,18 +1,33 @@
 import React from 'react'
 import './Chatbox.css'
-const Chatbox = (props) => (
-     <div className="messages-c">
+
+class Chatbox extends React.Component{
+     constructor(props) {
+          super(props);
+        }
+     componentDidUpdate() {
+          this.newData.scrollIntoView({ behavior: "smooth" })
+      }
+     render(){
+          return(
+               <div className="messages-c">    
+          <div className="chatlist">
+         {this.props.chatHistory.map((obj, index) => ( <p ref={(ref) => this.newData = ref}  key={index} className={obj.isEliza === true ? 'bot': 'user'}>{obj.content}</p>))}
+               </div>
+          </div>
+          )
+     }
+}
+// const Chatbox = (props) => (
+//      <div className="messages-c">
 
     
-    <div className="messages">
+//     <div className="messages">
     
-         {
-         props.chatHistory.map(obj => (
-          
-                <p className={obj.isEliza === true ? 'bot': 'user'}>{obj.content}</p>
-         ))}
-    </div>
-    </div>
-     ); 
+//          {props.chatHistory.map((obj, index) => ( <p key={index} className={obj.isEliza === true ? 'bot': 'user'}>{obj.content}</p>))}
+//     </div>
+//     </div>
+//      );
+     
 
 export default Chatbox
